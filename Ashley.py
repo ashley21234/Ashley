0927d04f51129fae4a712b303dfff2fc66005605
ashley21234 <fashy3@gmail.com>
2025-08-21 09:54:50 +0200

    Add files via upload

---
+++ Ashley.py
@@ -0,0 +1,20 @@
+# Constants (by convention, constants are written in UPPERCASE)
+PI = 3.14159
+GRAVITY = 9.8
+
+# Variables
+radius = 5
+height = 10
+
+# Calculate the area of a circle
+circle_area = PI * radius ** 2
+
+# Calculate the volume of a cylinder
+cylinder_volume = circle_area * height
+
+# Print results
+print("Radius:", radius)
+print("Height:", height)
+print("Area of Circle:", circle_area)
+print("Volume of Cylinder:", cylinder_volume)
+print("Gravity constant:", GRAVITY)
